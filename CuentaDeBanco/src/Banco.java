95ca5d4c3e415e48883916ae8721e1d0095150e1
jsusdvid <82040864+jsusdvid@users.noreply.github.com>
2021-04-08 08:52:30 -0400

    Update Banco.java

--- CuentaDeBanco/src/Banco.java
+++ CuentaDeBanco/src/Banco.java
@@ -7,13 +7,13 @@ public class Banco {
 		 String nombre, numero;
 		 double tipo, importe;
 
-		 //se crea objeto cuenta1 sin parámetros
+		 //se crea objeto cuenta1 sin parÃ¡metros
 		 //se ejecuta el constructor por defecto
 		 Cuenta cuenta1 = new Cuenta();
 
 		 System.out.print("Nombre : ");
 		 nombre = sc.nextLine();
-		 System.out.print("Número de cuenta : ");
+		 System.out.print("NÃºmero de cuenta : ");
 		 numero = sc.nextLine();
 		 System.out.print("Tipo de interes : ");
 		 tipo = sc.nextDouble();
@@ -26,8 +26,8 @@ public class Banco {
 		 cuenta1.setSaldo(importe);
 
 		 //se crea el objeto cuenta2 con los valores leidos por teclado
-		 //se ejecuta el constructor con parámetros
-		 Cuenta cuenta2 = new Cuenta("Juan Ferrández Rubio", "12345678901234567890", 1.75, 300);                       
+		 //se ejecuta el constructor con parÃ¡metros
+		 Cuenta cuenta2 = new Cuenta("Jesus David", "213546545613213546454656", 1.75, 300);                       
 
 		 //se crea cuenta3 como copia de cuenta1
 		 //se ejecuta el constructor copia
@@ -36,34 +36,34 @@ public class Banco {
 		 //mostrar los datos de cuenta1
 		 System.out.println("Datos de la cuenta 1");
 		 System.out.println("Nombre del titular: " + cuenta1.getNombre());
-		 System.out.println("Número de cuenta: " + cuenta1.getNumeroCuenta());
-		 System.out.println("Tipo de interés: " + cuenta1.getTipoInteres());
+		 System.out.println("NÃºmero de cuenta: " + cuenta1.getNumeroCuenta());
+		 System.out.println("Tipo de interÃ©s: " + cuenta1.getTipoInteres());
 		 System.out.println("Saldo: " + cuenta1.getSaldo());
 		 System.out.println();
 		       
 		 //se realiza un ingreso en cuenta1
 		 cuenta1.ingreso(4000);
 		     
-		 //mostrar el saldo de cuenta1 después del ingreso
+		 //mostrar el saldo de cuenta1 despuÃ©s del ingreso
 		 System.out.println("Saldo: " + cuenta1.getSaldo());
 		     
 		 //mostrar los datos de cuenta2
 		 System.out.println("Datos de la cuenta 2");
 		 System.out.println("Nombre del titular: " + cuenta2.getNombre());
-		 System.out.println("Número de cuenta: " + cuenta2.getNumeroCuenta());
-		 System.out.println("Tipo de interés: " + cuenta2.getTipoInteres());
+		 System.out.println("NÃºmero de cuenta: " + cuenta2.getNumeroCuenta());
+		 System.out.println("Tipo de interÃ©s: " + cuenta2.getTipoInteres());
 		 System.out.println("Saldo: " + cuenta2.getSaldo());
 		 System.out.println();
 		     
 		 //mostrar los datos de cuenta3
 		 System.out.println("Datos de la cuenta 3");
 		 System.out.println("Nombre del titular: " + cuenta3.getNombre());
-		 System.out.println("Número de cuenta: " + cuenta3.getNumeroCuenta());                                         
-		 System.out.println("Tipo de interés: " + cuenta3.getTipoInteres());
+		 System.out.println("NÃºmero de cuenta: " + cuenta3.getNumeroCuenta());                                         
+		 System.out.println("Tipo de interÃ©s: " + cuenta3.getTipoInteres());
 		 System.out.println("Saldo: " + cuenta3.getSaldo());
 		 System.out.println();
 		     
-		 //realizar una transferencia de 10€ desde cuenta3 a cuenta2
+		 //realizar una transferencia de 10â‚¬ desde cuenta3 a cuenta2
 		 cuenta3.transferencia(cuenta2, 10);
 		     
 		 //mostrar el saldo de cuenta2
